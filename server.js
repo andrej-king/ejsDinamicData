0c329e8a4787d59c26bb3744a15388bb1d32d453
Andrey <graduadu@gmail.com>
2020-09-30 09:22:14 +0300

    write Object on ejs, include css

--- server.js
+++ server.js
@@ -2,20 +2,30 @@ const express   = require('express');
 const ejs       = require('ejs');
 const app       = express();
 
+app.use(express.static('public')); // open folder for show
 app.set('view engine', ejs);
 
 app.get('/date', (req, res) => {
     let today = new Date();
     let day = '';
+    let personData = {
+        firstName: 'John',
+        lastName: 'Doe',
+        age: 34,
+        job: 'Developer'
+    }
 
     // weekdays
     if(today.getDay() === 6 || today.getDate === 0) {
-        day = 'Weekend!';
+        day = 'Weekend';
     } {
-        day = 'Workday!';
+        day = 'Workday';
     }
 
-    res.render('index.ejs', {kindOfDay: day}); // default find in folder views
+    res.render('index.ejs', {
+        kindOfDay: day,
+        person: personData
+    }); // default find in folder views
 });
 
 app.listen(3000, () => {
